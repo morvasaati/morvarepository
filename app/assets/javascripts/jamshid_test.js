57f4c5a9436615230eb9132002e3ff14d1cbe757
morvasaati <s_morva@yahoo.com>
2015-11-30 17:02:48 +0330

    dsadsfesfdsdsds

---
+++ app/assets/javascripts/jamshid_test.js
@@ -0,0 +1,16 @@
+
+jQuery(document).ready(function($) {
+
+    /*
+        Background slideshow
+    */
+    $.backstretch([
+      "/assets/fina2l2.jpg"
+    , "/assets/14.jpg"
+    , "/assets/14.jpg"
+    ], {duration: 100, fade: 750});
+
+
+});
+
+
